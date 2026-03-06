e94dd9c730212868f6a4924f6e325a32d5cfd853
sameerFront <neutron.02f@gmail.com>
2026-03-06 19:22:04 +0300

    f

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,7 +4,7 @@ import router from "@/router";
 const TOKEN_KEY = "nh_admin_token";
 
 const Api = axios.create({
-  baseURL: "http://localhost/NextHouse-Back/public/api",
+  baseURL: "http://nexthouse.it.com/public/api",
 });
 
 Api.defaults.withCredentials = false;
