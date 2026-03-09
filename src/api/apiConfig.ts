4f853619ef4d7516743d19325bcc25b215c015bd
sameerFront <neutron.02f@gmail.com>
2026-03-09 22:15:50 +0300

    fixing admin

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,7 +4,7 @@ import router from "@/router";
 const TOKEN_KEY = "nh_admin_token";
 
 const Api = axios.create({
-  baseURL: "https://nexthouse.it.com/api",
+  baseURL: "https://api.nexthouse.it.com/api",
 });
 
 Api.defaults.withCredentials = false;
